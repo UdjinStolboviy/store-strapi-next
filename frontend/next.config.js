49ed152b42d91a53b066ae98dbfed0717b894efc
Yvgen Stolbovyii <stolbovyii@mydigicode.com>
2023-04-19 13:07:09 +0200

    add fix

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -6,8 +6,8 @@ const nextConfig = {
     domains: [
       "localhost:8082",
       "localhost",
-      "https://5fb9-88-18-249-250.ngrok-free.app",
-      "5fb9-88-18-249-250.ngrok-free.app",
+      "https://559c-88-18-249-250.ngrok-free.app",
+      "559c-88-18-249-250.ngrok-free.app",
     ],
   },
 };
